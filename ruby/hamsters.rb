17684cb8edb5cfc8c397c5750bbb4ad874305b21
Abraham Kwak <kwak.abe@gmail.com>
2017-05-09 14:04:43 -0400

    deleted the double pseudo code in the hamsters.rb file.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -7,15 +7,6 @@
 # * if hamster is a good candidate for adoption
 # * hamster's estimated age (if age is not given use nil)
 
-# Hamster Sanctuary Project
-
-# Ask the user for the following information:
-# * hamster's name (name is given to hamster's without a name)
-# * hamster's volume level from 1 - 10 (1 being softest, 10 being the loudest)
-# * hamster's fur color
-# * if hamster is a good candidate for adoption
-# * hamster's estimated age (if age is not given use nil)
-
 puts "What is the hamster's name?"
 ham_name = gets.chomp
 puts "How loud is the hamster? (from a scale of 1 - 10, 10 being the loudest)"
