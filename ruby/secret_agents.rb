6d29205710a942de091ce457db6749c8786deede
Abraham Kwak <kwak.abe@gmail.com>
2017-05-11 21:42:02 -0400

    updated syntax and pseudo code for better readability.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,23 +1,22 @@
 # Encrypt Pseudo Code
 # define a method called encrypt.
 # create a variable called index and assign it a value of 0.
-# then create a while loop with a conditional statement index is less than the length of the input
-# if the letter 'z' is passed into argument chop off the next letter in the sequence
-# otherwise find the index of the input and set it to the next letter using the next method with a bang operator to mutate it.
-# add one to index to go to the next letter
-# print the input
+# create a while loop where it will go through each index position in the password and advance the character.
+# put in an if clause which removes an added letter when the letter 'z' is used.
+# add one to index to go to the next letter in the index position.
+# return the input.
 
-def encrypt(pass)
+def encrypt(password)
   index = 0
-  while index < pass.length
-    if pass[index] == "z"
-      pass[index] = pass[index].next!.chop
+  while index < password.length
+    if password[index] == "z"
+      password[index] = password[index].next!.chop
     else
-      pass[index] = pass[index].next!
+      password[index] = password[index].next!
     end
     index += 1
   end
-  pass
+  password
 end
 
 # Decrypt Pseudo Code
@@ -25,19 +24,19 @@ end
 # create a variable called string which encases the alphabet.
 # create a variable called index and assign it a value of 0.
 # then create a while loop with a conditional statement index is less than the length of the input
-# use the index method on a letter in our pass and take the index and apply it to the string
-# reduce the index by 1 for each character and take the result and apply it to pass.
+# use the index method on a letter in our password and take the index and apply it to the string
+# reduce the index by 1 for each character and take the result and apply it to password.
 # add one to index to go to the next letter
 # print the input
 
-def decrypt(pass)
-  string = "abcdefghijklmnopqrstuvwxyz"
+def decrypt(password)
+  alpha = "abcdefghijklmnopqrstuvwxyz"
   index = 0
-  while index < pass.length
-    pass[index] = string[string.index(pass[index]) - 1]
+  while index < password.length
+    password[index] = alpha[alpha.index(password[index]) - 1]
     index += 1
   end
-  pass
+  password
 end
 
 # p encrypt("abc")
@@ -62,10 +61,10 @@ until pass_type == "encrypt" || pass_type == "decrypt"
 end
 
 puts "Please enter your password."
-p_word = gets.chomp.downcase
+pass_word = gets.chomp.downcase
 
 if pass_type == "encrypt"
-  p encrypt(p_word)
+  p encrypt(pass_word)
 else
-  p decrypt(p_word)
+  p decrypt(pass_word)
 end
