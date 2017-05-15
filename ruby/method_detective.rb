4be6b23016d110c8a70e7a65b7b87c8fc91cf323
Abraham Kwak <kwak.abe@gmail.com>
2017-05-15 12:17:10 -0400

    completed assignment 5.1 by finding all correct built-in class String methods.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -7,6 +7,7 @@
 "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
+"zom".sub("o", "oo")
 "zom".gsub("o", "oo")
 # => “zoom”
 
@@ -35,9 +36,10 @@
 "Elementary,    my   dear        Watson!".gsub(/\s+/, ' ')
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122
 # (What is the significance of the number 122 in relation to the character z?)
+# The significance of the number is based on ASCII code table which assigns an ordinal position to a character, so the letter 'z' has an ordinal position of 122. Take for example the letter capital 'A' is position 65 while the letter capital 'B' is 66 since it comes after the letter capital 'A' so it's viewed as it's successor. Following these ordinal positions letters that come after are 'higher' or are considered 'greater'. When we do a comparison of 'Z' > 'z' you will get a 'false' this is because the lowercase letter 'z' comes later and has a higher ordinal position integer.
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
