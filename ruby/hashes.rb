c8c883324cf1526e68740da52b8080e61a1d6300
Abraham Kwak <kwak.abe@gmail.com>
2017-05-15 19:40:32 -0400

    created pseudo code for interior designer client info. application on the hashes.rb file.

---
+++ ruby/hashes.rb
@@ -0,0 +1,4 @@
+# Interior Designer Client Info Application Pseudo Code
+# Collect client's information which include the client's name,age,number of children they have,decor theme, favorite color, their spending budget and if they have pets.
+# Once all information has been collected, display it.
+# In the case the user has entered incorrect information on any of the questions allow the user to re-enter the correct information.
