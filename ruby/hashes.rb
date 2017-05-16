c02c7a20d1e918cdf9019ffb88b7b0f3be94f5cd
Abraham Kwak <kwak.abe@gmail.com>
2017-05-15 20:32:05 -0400

    updated pseudo code, created user input code and code to re-enter information that was incorrect.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,4 +1,47 @@
 # Interior Designer Client Info Application Pseudo Code
-# Collect client's information which include the client's name,age,number of children they have,decor theme, favorite color, their spending budget and if they have pets.
+# Collect client's information which include the client's name, age, number of children they have, decor theme, favorite color, their spending budget and if they have pets.
+# In the case the user has entered incorrect information on any of the questions, allow the user to re-enter the correct information.
 # Once all information has been collected, display it.
-# In the case the user has entered incorrect information on any of the questions allow the user to re-enter the correct information.
+
+
+client_info = {
+  client_name: "",
+  client_age: "",
+  client_children: "",
+  client_decor: "",
+  client_color: "",
+  client_budget: "",
+  client_pets: false
+}
+
+puts "Welcome to the Client Information Application for Interior Designers."
+
+puts "Please enter your client's name."
+client_info[:client_name] = gets.chomp
+
+puts "Please enter your client's age."
+client_info[:client_age] = gets.chomp.to_i
+
+puts "Please enter the number of children your client has."
+client_info[:client_children] = gets.chomp.to_i
+
+puts "Please enter the decor theme your client desires."
+client_info[:client_decor] = gets.chomp
+
+puts "Please enter your client's favorite color."
+client_info[:client_color] = gets.chomp
+
+puts "Please enter your client's spending budget for the project."
+client_info[:client_budget] = gets.chomp.to_i
+
+puts "Please enter your if your client has pets.(y/n)"
+client_own_pets = gets.chomp
+
+if client_own_pets == 'y'.downcase
+  client_info[:client_pets] = true
+else
+  client_info[:client_pets]
+end
+
+puts "Is there any information you need to update? Enter 'client_name', 'client_age', 'client_children', 'client_decor', 'client_color', 'client_budget', 'client_pets'. If not type 'none'."
+client_update = gets.chomp.to_sym
