0f11f10b075918009b74a24ea42d2b75ba6312a9
Abraham Kwak <kwak.abe@gmail.com>
2017-05-16 21:25:21 -0400

    created nested data structure.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,42 @@
+attack_bot = {
+  head: {
+    brain: {
+      left_brain: 'super logic chip',
+      right_brain: 'super creative chip'
+    },
+    left_eye: 'laser beam',
+    right_eye: 'eye ball missle',
+    nose: 'steel nose',
+    mouth: ['metal teeth', 'spiked tongue'],
+  },
+  body: {
+    heart_type: 'nuclear',
+    stomach: {
+      large_intestine: 'gas',
+      small_intestine: 'oil'
+    },
+  },
+  left_arm: {
+    shoulder: 'rocket launcher',
+    elbow: 'spike launcher',
+    hand: {
+      fingers: ['thumb', 'index', 'middle', 'ring', 'pinky']
+    }
+  },
+  right_arm: {
+    shoulder: 'rocket launcher',
+    elbow: 'spike launcher',
+    hand: {
+      fingers: ['thumb', 'index', 'middle', 'ring', 'pinky']
+    }
+  },
+  left_leg: {
+    knee: 'grenade launcher',
+    foot: ['big toe', 'long toe', 'middle toe', 'ring toe', 'little toe']
+  },
+  right_leg: {
+    knee: 'grenade launcher',
+    foot: ['big toe', 'long toe', 'middle toe', 'ring toe', 'little toe']
+  },
+  skin: 'platinum'
+}
