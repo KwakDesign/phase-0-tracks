f413bab7908ffc13648c3f0250abaf5505c9256d
Abraham Kwak <kwak.abe@gmail.com>
2017-05-17 12:56:38 -0400

    initialized pseudo code for the alias manager application project.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,1 +1,6 @@
 # Alias Manager Application Pseudo Code
+# Collect user's first name and last name and capitalize each first letter if not capitalized already.
+# Swap first name and last name.
+# Iterate through each character in first name and change all vowels(a,e,i,o,u) to the next vowel in 'aeiou' and all consonants to the next consonant in the alphabet.
+# Then return the updated the updated first and last name.
+
