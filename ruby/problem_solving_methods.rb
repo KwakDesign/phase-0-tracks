10e5c95b4608c712b5bc2bb005bb2c5601ac1bba
Abraham Kwak <kwak.abe@gmail.com>
2017-05-18 13:30:55 -0400

    created calculate fibonacci numbers method and completed release 1.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,4 @@
+# Release 0: Simple Search
 numbers = [11,23,37,42,51]
 def search_array(arr, int)
   index = 0
@@ -8,5 +9,17 @@ def search_array(arr, int)
     index += 1
   end
 end
-
 search_array(numbers, 99)
+
+# Release 1: Calculate Fibonacci Numbers
+def fib(num)
+  fib_arr = [0,1]
+  # last_num = fib_arr[-1]
+  # second_to_last = fib_arr[-2]
+  # next_num = last_num + second_to_last
+  (num - 2).times do |num|
+    fib_arr.push(fib_arr[-1] + fib_arr[-2])
+  end
+  p fib_arr.last
+end
+fib(100)
