87162c5d6d078e11fe4b96e076232c9cb6ffae7f
Abraham Kwak <kwak.abe@gmail.com>
2017-05-17 20:36:12 -0400

    refactored vowel and consonant advance method into single alias modifier method that handles upper and lowercase characters.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,12 +7,24 @@
 puts "Please enter your name."
 name = gets.chomp
 
-def vowel_adv(name)
-  vowels = ["a", "e", "i", "o", "u"]
+puts "Please enter your name."
+name = gets.chomp
+
+def alias_modifier(name)
+  low_vowels = ["a", "e", "i", "o", "u"]
+  up_vowels = ["A", "E", "I", "O", "U"]
+  low_consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+  up_consonants = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
   name = name.split('')
   updated_name = name.map do |char|
-    if vowels.include?(char)
-      vowels.rotate[vowels.index(char)]
+    if low_vowels.include?(char)
+      low_vowels.rotate[low_vowels.index(char)]
+    elsif up_vowels.include?(char)
+      up_vowels.rotate[up_vowels.index(char)]
+    elsif low_consonants.include?(char)
+      low_consonants.rotate[low_consonants.index(char)]
+    elsif up_consonants.include?(char)
+      up_consonants.rotate[up_consonants.index(char)]
     else
       char
     end
@@ -20,21 +32,7 @@ def vowel_adv(name)
   updated_name.join
 end
 
-vowel_name = vowel_adv(name)
+alias_modifier(name).split(' ').reverse.join(' ')
 
-def consonant_adv(vowel_name)
-  consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-  vowel_name = vowel_name.split('')
-  conson_name = vowel_name.map do |char|
-    if consonants.include?(char)
-      consonants.rotate[consonants.index(char)]
-    else
-      char
-    end
-  end
-  conson_name.join
-end
-
-consonant_adv(vowel_name)
 
 
