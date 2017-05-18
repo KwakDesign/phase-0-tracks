ef12d9e2071705dcd110ece002036a3c6c6c1760
Abraham Kwak <kwak.abe@gmail.com>
2017-05-18 12:57:42 -0400

    created search array method and completed release 1.

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,12 @@
+numbers = [11,23,37,42,51]
+def search_array(arr, int)
+  index = 0
+  while index < arr.length
+    if int == arr[index]
+      p index
+    end
+    index += 1
+  end
+end
+
+search_array(numbers, 99)
