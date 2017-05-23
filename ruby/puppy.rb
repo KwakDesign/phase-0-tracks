2bc68f40b7408872c8b9077011c5dc3843ff5a1f
Abraham Kwak <kwak.abe@gmail.com>
2017-05-23 17:00:23 -0400

    designed Puppy class and added characteristics and behavior.

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+# Species:
+# Canis
+
+# Characteristics:
+# Friendly: varies
+# Eye count: 2
+# Color: varies
+# Fur type: varies
+# Name: varies
+
+# Behavior:
+# Playful
+# Empathetic
+# Submissive
+# Agressive
+# Curiosity/Fearful
+# Reactivity
+# Responsiveness
+# Activity
