f2268bec531c62e6f48a3f9427e2eb6c6611e4b8
Abraham Kwak <kwak.abe@gmail.com>
2017-05-24 18:02:38 -0400

    adding initialization with a loop and calling instance methods on each instance.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,3 +14,24 @@ class Santa
     puts "That was a good #{cookie_type}!"
   end
 end
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+santa_group = []
+santa_genders = ["agender", "bigender", "female", "gender fluid", "male", "pangender", "N/A"]
+santa_ethnicities = ["African-American", "Asian/Pacific-Islander", "Caucasian", "Latino", "South Asian", "Mystical Creature (unicorn)", "N/A"]
+
+santa_genders.length.times do |i|
+  santa_group << Santa.new(santa_genders[i], santa_ethnicities[i])
+  santa_group.each_with_index do |santa, idx|
+    puts santa.speak
+    break
+  end
+end
