0230ce0d04d37135b055b6147c1a24e17cac9c1d
Abraham Kwak <kwak.abe@gmail.com>
2017-05-26 16:23:42 -0400

    created add item and delete item instance methods inside the TodoList class and ran successful rspec test.

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -6,4 +6,14 @@ class TodoList
   def get_items
     @list
   end
+
+  def add_item(item_name)
+    @list << item_name
+  end
+
+  def delete_item(item_name)
+    @list.delete(item_name)
+  end
 end
+
+todolist = TodoList.new(["do the dishes", "mow the lawn"])
