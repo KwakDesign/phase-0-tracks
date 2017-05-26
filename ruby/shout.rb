36144244cc162e418558d20445ae3227b30a4cb4
Abraham Kwak <kwak.abe@gmail.com>
2017-05-25 20:57:39 -0400

    declared Shout module, loaded it using require relative and created module methods and called them.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
 
+  def self.yelling_happily(words)
+    "WHOO!!!" + words + "!!!" + " :D"
+  end
 end
+
+puts Shout.yell_angrily("THE HECK MAN")
+puts Shout.yelling_happily("OH YEAH MANE")
