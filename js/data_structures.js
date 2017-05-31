04ee17d5ee25c0a50f13010e199e8869213b56b9
Abraham Kwak <kwak.abe@gmail.com>
2017-05-31 16:20:06 -0400

    created data_structure.js file, created 2 arrays called colors and names and used push methods to add new items to each array.

---
+++ js/data_structures.js
@@ -0,0 +1,6 @@
+// Created colors array and names array
+var colors = ["red", "blue", "yellow", "green"];
+var names = ["Ed", "Bob", "Ted", "Eric"];
+// Used .push method to add in new values to each array
+colors.push("orange");
+names.push("Joe");
