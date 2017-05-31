cca67f3954c04467faffa81d9e376728098f297b
Abraham Kwak <kwak.abe@gmail.com>
2017-05-31 15:58:36 -0400

    created object called horse and applied the for loop to go through the color and name arrays to fill in the key and values for the object.

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,3 +2,8 @@ var colors = ["red", "blue", "yellow", "green"];
 var names = ["Ed", "Bob", "Ted", "Eric"];
 colors.push("orange");
 names.push("Joe");
+
+var horse = {};
+for (var i = 0; i < names.length; i++){
+  horse[names[i]] = colors[i];
+}
