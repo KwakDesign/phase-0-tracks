4c5c48b024db4982291a7afaa5e55fa15bc5ff8f
Abraham Kwak <kwak.abe@gmail.com>
2017-05-31 16:21:36 -0400

    created horse object and used a for loop to add values from the color and names array as the horse objects keys and values.

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,3 +4,11 @@ var names = ["Ed", "Bob", "Ted", "Eric"];
 // Used .push method to add in new values to each array
 colors.push("orange");
 names.push("Joe");
+
+// Created horse object and used a for loop to add the values from the 2 arrays into the object.
+var horse = {};
+for (var i = 0; i < names.length; i++){
+  horse[names[i]] = colors[i];
+}
+
+console.log(horse);
