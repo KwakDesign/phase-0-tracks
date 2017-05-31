55aebe96d0e111d61c9f621c3a20e9a2362f66d1
Abraham Kwak <kwak.abe@gmail.com>
2017-05-31 11:56:41 -0400

    created pseudo code for reverse string function.

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+// create a function called reverse string that takes a string as a parameter.
+  // create an new empty variable which will eventually store the updated reversed string.
+  // create a for loop where it's counter variable starts at the last index position of the string, while the index position is greater than 0, move backwards through each index position.
+    // store each character at the index position into the empty variable.
+    // print the variable with the reversed string.
