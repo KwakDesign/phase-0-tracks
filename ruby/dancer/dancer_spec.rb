71327f15c990cec12370fc53362588900252f249
Abraham Kwak <kwak.abe@gmail.com>
2017-06-01 12:57:00 -0400

    created dancer.rb with updated code and udpated code for dancer rspec file.

--- ruby/dancer/dancer_spec.rb
+++ ruby/dancer/dancer_spec.rb
@@ -1,13 +1,7 @@
+require_relative 'dancer'
+
 describe Dancer do
   let(:dancer) { Dancer.new("Misty Copeland", 33) }
-
-  it "pirouettes" do
-    expect(dancer.pirouette).to eq "*twirls*"
-  end
-
-  it "bows to a partner" do
-    expect(dancer.bow).to eq "*bows*"
-  end
 
   it "has a readable name" do
     expect(dancer.name).to eq "Misty Copeland"
@@ -22,18 +16,40 @@ describe Dancer do
     expect(dancer.age).to eq 34
   end
 
-  it "keeps a readable dance card" do
-    expect(dancer.card).to eq []
+  it "twirls" do
+    expect(dancer.pirouette).to eq "*twirls*"
   end
 
-  it "can queue parters on the dance card" do
+  it "bows to a partner" do
+    expect(dancer.bow).to eq "*bows*"
+  end
+
+  it "leaps" do
+    expect(dancer.leap).to eq "*leaps*"
+  end
+
+  it "can change the dancers tutu color" do
+    dancer.tutu_color = "black"
+    expect(dancer.tutu_color).to eq ("black")
+  end
+
+  it "says there are too many dancers waiting on queue" do
+    dancer.queue_dance_with("Mikhail Baryshnikov")
+    dancer.queue_dance_with("Anna Pavlova")
+    dancer.queue_dance_with("Nina Sayers")
+    dancer.queue_dance_with("Lily")
+    dancer.queue_dance_with("Bob")
+    expect(dancer.queue_dance_with("David")).to eq "There are too many dancers who have signed up. Please wait."
+  end
+
+  it "can add parters to the dance card queue" do
     dancer.queue_dance_with("Mikhail Baryshnikov")
     expect(dancer.card).to eq ["Mikhail Baryshnikov"]
     dancer.queue_dance_with("Anna Pavlova")
     expect(dancer.card).to eq ["Mikhail Baryshnikov", "Anna Pavlova"]
   end
 
-  it "can start next dance from queue" do
+  it "can start the next dance in the queue" do
     dancer.queue_dance_with("Mikhail Baryshnikov")
     dancer.queue_dance_with("Anna Pavlova")
     expect(dancer.begin_next_dance).to eq "Now dancing with Mikhail Baryshnikov."
