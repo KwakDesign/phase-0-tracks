33b590b85343c1f077b0ca267ecbf9cb87300ade
Abraham Kwak <kwak.abe@gmail.com>
2017-06-03 11:59:07 -0400

    refactored the predicted_deaths and speed_of_spread instance methods.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -17,31 +17,30 @@ class VirusPredictor
     @population_density = population_density
   end
 
-  # the private keyword makes the following lines of code exclusive to this class
-  private
-
   # combination of calling the two main instance methods of this class VirusPredictor.
   def virus_effects
     predicted_deaths
     speed_of_spread
   end
 
+  # the private keyword makes the following lines of code exclusive to this class
+  private
 
   # categorizes the states based off of the population density and calculates the number of deaths based on which category condition it is.
   def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      factor = (0.4)
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      factor = (0.3)
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      factor = (0.2)
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      factor = (0.1)
     else
-      number_of_deaths = (@population * 0.05).floor
+      factor = (0.05)
     end
-
+    number_of_deaths = (@population * factor).floor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
@@ -53,15 +52,15 @@ class VirusPredictor
     speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
