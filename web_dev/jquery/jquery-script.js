216ca0e2bfc6907c23078464c09ea6b848b59b5e
Abraham Kwak <kwak.abe@gmail.com>
2017-06-15 13:27:01 -0400

    created style.css file and implemented styles, updated index.html file, and implemented jQuery code in jquery-script.js file completing release 2.

---
+++ web_dev/jquery/jquery-script.js
@@ -0,0 +1,8 @@
+$(function() {
+  // hide img and h2.
+  $('.img-container, h2').hide();
+  // assign click method to fade in img and h2 slowly.
+  $('h1').click(function() {
+    $('.img-container, h2').fadeIn(3000);
+  });
+});
