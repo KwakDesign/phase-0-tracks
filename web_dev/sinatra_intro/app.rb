56bd83172954405e2305cc6b2dee46f9e328b9ca
Abraham Kwak <kwak.abe@gmail.com>
2017-06-15 21:48:55 -0400

    created /contact route, /great_job route, route that adds two numbers and the optional route that searches the database in some way, completing release 0.

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -39,8 +39,45 @@ end
 
 # write a GET route that retrieves
 # a particular student
-
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a GET route that retrieves an address
+get '/address/:address' do
+  address = params[:address]
+  "My address is #{params[:address]}."
+end
+
+# write a GET route that retrieves a person's name and say 'Good job, [person's name]!'
+get '/great_job/' do
+  person_name = params[:person_name]
+  if person_name
+    "Good job, #{params[:person_name]}!"
+  else
+    "Good job!"
+  end
+end
+
+# write GET route parameters to add two numbers and return the result.
+get '/:num_1/plus/:num_2' do
+  sum = "#{params[:num_1]}".to_i + "#{params[:num_2]}".to_i
+  sum.to_s
+end
+
+# write a GET route parameter that allows you to search for the names of students going to the campus in NYC.
+get '/students/campus/:campus' do
+  students = db.execute("SELECT name FROM students WHERE campus=?", [params[:campus]])
+  response = ''
+  students.each do |student|
+    response << "#{student['name']} goes to the NYC campus.<br><br>"
+  end
+  response
+end
+
+# multi parameter version, here we can retrieve students from two parameters. i.e. /students/age/62 or /students/campus/NYC
+# get '/students/:param/:match' do
+#   student = db.execute("SELECT * FROM students WHERE #{params[:param]}=?", [params[:match]])
+#   student.to_s
+# end
